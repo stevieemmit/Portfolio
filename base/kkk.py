3a64396a9872eda732e3a51bbd8638595d19a521
Alagbe Boluwatife Steven <33502625+stevieemmit@users.noreply.github.com>
2022-06-08 02:44:04 +0100

    Initial Commit

---
+++ base/kkk.py
@@ -0,0 +1,14 @@
+
+def hello():
+    name=str(input("enter the name : "))
+    num1= input("How many times: ")
+    for i in word: print("hello " + str(name ))
+    
+
+    return
+hello()
+
+word = input('What is your name ')
+number = int(input('Enter a number ')
+
+
