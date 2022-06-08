3c06ca1489a90251bec201553a480ecb49b0ac61
Alagbe Boluwatife Steven <33502625+stevieemmit@users.noreply.github.com>
2022-06-08 03:34:49 +0100

    2nd commit

--- dennisivy/settings.py
+++ dennisivy/settings.py
@@ -32,12 +32,14 @@ ALLOWED_HOSTS = ['127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 
     'base.apps.BaseConfig',
 
